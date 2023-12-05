faf6b9fb929496be3839fce0a2d4e42adc97f22a
phucpro746 <phucpro746@gmail.com>
2023-12-05 11:00:39 +0700

    Add global handler exception

---
+++ src/infrastructures/Gara.Exceptions/Filters/HttpGlobalExceptionFilter.cs
@@ -0,0 +1,61 @@
+namespace Gara.Exceptions.Filters
+{
+    using Gara.Exceptions.ActionResults;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+    using Microsoft.Extensions.Logging;
+    using System.Net;
+
+    /// <inheritdoc />
+    /// <summary>
+    ///   The handle exception.
+    /// </summary>
+    public partial class HttpGlobalExceptionFilter : IExceptionFilter
+    {
+        private readonly ILogger<HttpGlobalExceptionFilter> _logger;
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref="HttpGlobalExceptionFilter" /> class.
+        /// </summary>
+        /// <param name="env">The web host enviroment.</param>
+        /// <param name="logger">The logger.</param>
+        public HttpGlobalExceptionFilter(ILogger<HttpGlobalExceptionFilter> logger)
+        {
+            this._logger = logger ?? throw new System.ArgumentNullException(nameof(logger));
+        }
+
+        /// <inheritdoc />
+        public void OnException(ExceptionContext context)
+        {
+            _logger.LogError(
+                new EventId(context.Exception.HResult),
+                context.Exception,
+                context.Exception.Message);
+
+            if (context.Exception.GetType() == typeof(BaseDomainException))
+            {
+                var json = new JsonErrorResponse
+                {
+                    Messages = new[] { context.Exception.Message },
+                };
+
+                context.Result = new BadRequestObjectResult(json);
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            }
+            else
+            {
+                var json = new JsonErrorResponse
+                {
+                    Messages = new[] { "An error occurred. Try it again." },
+                };
+
+                json.DeveloperMessage = context.Exception;
+
+                context.Result = new InternalServerErrorObjectResult(json);
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            }
+
+            context.ExceptionHandled = true;
+        }
+    }
+}
---
+++ src/infrastructures/Gara.Exceptions/Filters/ValidateModelFilter.cs
@@ -0,0 +1,45 @@
+﻿using Gara.Domain.ServiceResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Gara.Exceptions.Filters
+{
+    public class ValidateModelFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var validationErrors = context.ModelState.Select((entry) =>
+                {
+                    var key = entry.Key;
+                    var err = context.ModelState[key].Errors.Select(e => e.ErrorMessage).ToArray();
+
+                    return new InvalidModel()
+                    {
+                        PropertyName = key,
+                        ErrorMessage = string.Join(";", err)
+                    };
+                });
+
+                var json = new BaseServiceResult() { ErrorMessages = validationErrors.Select(s => s.ToString()).ToList(), StatusCode = System.Net.HttpStatusCode.BadRequest };
+                context.Result = new BadRequestObjectResult(json);
+            }
+            else
+            {
+                await next();
+            }
+        }
+
+        private class InvalidModel
+        {
+            public string PropertyName { get; set; }
+            public string ErrorMessage { get; set; }
+
+            public override string ToString()
+            {
+                return $"{PropertyName}: {ErrorMessage}";
+            }
+        }
+    }
+}
