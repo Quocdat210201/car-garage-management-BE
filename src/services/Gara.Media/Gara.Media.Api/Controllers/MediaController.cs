7bab7a378865fcbcd75d0af520cdf6f277ca9e2e
phucpro746 <phucpro746@gmail.com>
2023-12-13 22:53:27 +0700

    Create Media api

---
+++ src/services/Gara.Media/Gara.Media.Api/Controllers/MediaController.cs
@@ -0,0 +1,48 @@
+﻿using Gara.Media.Api.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gara.Media.Api.Controllers
+{
+    [Route("api/media")]
+    public class MediaController : Controller
+    {
+        private readonly ILogger<MediaController> _logger;
+
+        public MediaController(ILogger<MediaController> logger)
+        {
+            _logger = logger ?? throw new System.ArgumentNullException(nameof(logger));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(string folder, string fileName, [FromForm] IFormFile file)
+        {
+            _logger.LogInformation($"Uploading the {fileName} to {folder}");
+            var t1 = Task.Run(() => FileHelper.SaveFile(folder, fileName, file));
+
+            await Task.WhenAll(t1);
+
+            var extension = Path.GetExtension(file.FileName);
+
+            return Ok(Url.Action("GetFile", "Media", new { folder = folder, fileName = string.Format("{0}{1}", fileName, extension) }, "https", "localhost:5001"));
+        }
+
+        [HttpGet("{folder}/{fileName}")]
+        public async Task<IActionResult> GetFile(string folder, string fileName)
+        {
+            var t1 = Task.Run(() => FileHelper.GetFile(folder, fileName));
+
+            await Task.WhenAll(t1);
+
+            var fileStream = System.IO.File.OpenRead(t1.Result);
+
+            var contentType = GetMimeType(fileName);
+
+            return base.File(fileStream, contentType);
+        }
+
+        private string GetMimeType(string fileName)
+        {
+            return MimeTypes.GetMimeType(fileName);
+        }
+    }
+}
