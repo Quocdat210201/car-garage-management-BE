2baa26a1bdbca2e60b298683e86a6fb7160d40f4
phucpro746 <phucpro746@gmail.com>
2024-01-07 15:41:28 +0700

    updated

--- src/services/Gara.Management/Gara.Management.Domain/Queries/AppointmentSchedules/AdminAppointmentScheduleListByStaffQuery.cs
+++ src/services/Gara.Management/Gara.Management.Domain/Queries/AppointmentSchedules/AdminAppointmentScheduleListByStaffQuery.cs
@@ -19,16 +19,24 @@ namespace Gara.Management.Domain.Queries.AppointmentSchedules
     public class AdminAppointmentScheduleListByStaffHandler : IRequestHandler<AdminAppointmentScheduleListByStaffQuery, ServiceResult>
     {
         private readonly IRepository<AppointmentSchedule> _repository;
+        private readonly IRepository<AppointmentScheduleDetail> _appointmentScheduleDetailsRepository;
 
-        public AdminAppointmentScheduleListByStaffHandler(IRepository<AppointmentSchedule> repository)
+        public AdminAppointmentScheduleListByStaffHandler(IRepository<AppointmentSchedule> repository,
+            IRepository<AppointmentScheduleDetail> appointmentScheduleDetailsRepository)
         {
             _repository = repository;
+            _appointmentScheduleDetailsRepository = appointmentScheduleDetailsRepository;
         }
 
         public async Task<ServiceResult> Handle(AdminAppointmentScheduleListByStaffQuery request, CancellationToken cancellationToken)
         {
             var result = new ServiceResult();
-            var data = await _repository.GetWithIncludeAsync(p => p.StaffId == request.StaffId, 0, 0, p => p.Staff, p => p.Car, p => p.Car.Owner);
+            var data = await _repository.GetWithIncludeAsync(p => p.StaffId == request.StaffId, 0, 0, p => p.Staff, p => p.Car, p => p.Car.Owner, p => p.AppointmentScheduleDetails);
+
+            foreach (var item in data)
+            {
+                item.AppointmentScheduleDetails = (List<AppointmentScheduleDetail>?)await _appointmentScheduleDetailsRepository.GetWithIncludeAsync(p => p.AppointmentScheduleId == item.Id, 0, 0, p => p.RepairService, p => p.AutomotivePartInWarehouse);
+            }
 
             result.Success(data);
             return result;
