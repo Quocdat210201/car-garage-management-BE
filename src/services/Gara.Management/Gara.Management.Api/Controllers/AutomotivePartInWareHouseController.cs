56148a724a7565abf874b97a1bb7d65168c95fc6
phucpro746 <phucpro746@gmail.com>
2024-01-16 08:51:37 +0700

    Updated

---
+++ src/services/Gara.Management/Gara.Management.Api/Controllers/AutomotivePartInWareHouseController.cs
@@ -0,0 +1,19 @@
+﻿using Gara.Management.Api.Constants;
+using Gara.Management.Domain.Queries.AutomotivePartInWareHouse;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gara.Management.Api.Controllers
+{
+    [Authorize(Policy = RolePolicy.STAFF_POLICY)]
+    [Route("api/automotive-part")]
+    public class AutomotivePartInWarehouseController : BaseApiController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllAutomotiveParts()
+        {
+            var result = await Mediator.Send(new AutomotivePartsInWarehouseListQuery());
+            return Ok(result);
+        }
+    }
+}
---
+++ src/services/Gara.Management/Gara.Management.Domain/Queries/AutomotivePartInWareHouse/AutomotivePartsInWareHouseListQuery.cs
@@ -0,0 +1,31 @@
+﻿using Gara.Domain.ServiceResults;
+using Gara.Management.Domain.Entities;
+using Gara.Persistance.Abstractions;
+using MediatR;
+
+namespace Gara.Management.Domain.Queries.AutomotivePartInWareHouse
+{
+    public class AutomotivePartsInWarehouseListQuery : IRequest<ServiceResult>
+    {
+    }
+
+    public class AutomotivePartsInWarehouseListQueryHandler : IRequestHandler<AutomotivePartsInWarehouseListQuery, ServiceResult>
+    {
+        private readonly IRepository<AutomotivePartInWarehouse> _repository;
+
+        public AutomotivePartsInWarehouseListQueryHandler(IRepository<AutomotivePartInWarehouse> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ServiceResult> Handle(AutomotivePartsInWarehouseListQuery request, CancellationToken cancellationToken)
+        {
+            var result = new ServiceResult();
+
+            var data = await _repository.GetWithIncludeAsync(null, 0, 0, a => a.AutomotivePart);
+
+            result.Success(data);
+            return result;
+        }
+    }
+}
